4141840314fc4dd6a9adc6c6eaae2d47e8243c35
mariaitit <mariafitz222@gmail.com>
2026-05-14 09:10:50 +0300

    task 3

--- task_3.py
+++ task_3.py
@@ -1,43 +1,43 @@
 class PointsForPlace:
-    def __init__(self):
-        self.points = 0
 
-    def get_points_for_place(self, place):
+    @staticmethod
+    def get_points_for_place(place):
+        points = 0
+
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
-            return self.points
+            return points
 
         if place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место')
-            return self.points
+            return points
 
-        self.points = 101 - place
-        return self.points
+        points = 101 - place
+        return points
 
 
 class PointsForMeters:
-    def __init__(self):
-        self.points = 0
 
-    def get_points_for_meters(self, meters):
+    @staticmethod
+    def get_points_for_meters(meters):
+        points = 0
+
         if meters < 0:
             print('Количество метров не может быть отрицательным')
-            return self.points
+            return points
 
-        self.points = meters * 0.5
-        return self.points
+        points = meters * 0.5
+        return points
 
 
 class TotalPoints(PointsForPlace, PointsForMeters):
-    def __init__(self):
-        PointsForPlace.__init__(self)
-        PointsForMeters.__init__(self)
 
     def get_total_points(self, meters, place):
-        total = self.get_points_for_place(place) + self.get_points_for_meters(meters)
+        total = (
+            self.get_points_for_place(place) +
+            self.get_points_for_meters(meters)
+        )
         return total
-
-
 
 
 points_for_place = PointsForPlace()
